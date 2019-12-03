76ea38f19c30e2ca815b079c7e3a2e26eeadbb49
Vishakha V <57158055+inaarakheraj@users.noreply.github.com>
2019-12-03 13:44:18 -0500

    added management commands for import through bash

--- tracking/management/commands/import_squirrel_data.py
+++ tracking/management/commands/import_squirrel_data.py
@@ -1,61 +1,45 @@
 from django.core.management.base import BaseCommand, CommandError
 from tracking.models import Sighting
 import datetime
+import pandas as pd
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'Syntax: python manage.py import_squirrel_data path/to/file.csv'
 
     def add_arguments(self, parser):
         parser.add_argument('path', nargs='+', type=str)
 
-    @staticmethod
-    def to_bool(s):
-        if s == 'TRUE':
-            return True
-        if s == 'FALSE':
-            return False
-
     def handle(self, *args, **options):
 
         path = options['path'][0]
-        f = open(path, 'r')
-        for row in f.readlines()[1:]:
-            row = row.split(',')
+        d = pd.read_csv(path)
+
+        for i, row in d.iterrows():
+            if i%100 == 0: print(i)
+
             s = Sighting()
-            s.Latitude = row[0]
-            s.Longitude = row[1]
-            s.Unique_Squirrel_ID = row[2]
-            s.Shift = row[4]
-            s.Date = datetime.date(int(row[5][4:]),int(row[5][:2]),int(row[5][2:4]))
-            s.Age = row[7]
-            s.Primary_Fur_Color = row[8]
-            s.Location = row[12]
-            s.Specific_Location = row[14]
-            s.Running = Command.to_bool(row[15])
-            s.Chasing = Command.to_bool(row[16])
-            s.Climbing = Command.to_bool(row[17])
-            s.Eating = Command.to_bool(row[18])
-            s.Foraging = Command.to_bool(row[19])
-            s.Other_Activities = row[20]
-            s.Kuks = Command.to_bool(row[21])
-            s.Quaas = Command.to_bool(row[22])
-            s.Moans = Command.to_bool(row[23])
-            s.Tail_flags = Command.to_bool(row[24])
-            s.Tail_twitches = Command.to_bool(row[25])
-            s.Approaches = Command.to_bool(row[26])
-            s.Indifferent = Command.to_bool(row[27])
-            s.Runs_from = Command.to_bool(row[28])
+            s.Latitude = row['X']
+            s.Longitude = row['Y']
+            s.Unique_Squirrel_ID = row['Unique Squirrel ID']
+            s.Shift = row['Shift']
+            s.Date = datetime.date(int(str(row['Date'])[4:]),int(str(row['Date'])[:2]),int(str(row['Date'])[2:4]))
+            s.Age = row['Age']
+            s.Primary_Fur_Color = row['Primary Fur Color']
+            s.Location = row['Location']
+            s.Specific_Location = row['Specific Location']
+            s.Running = row['Running']
+            s.Chasing = row['Chasing']
+            s.Climbing = row['Climbing']
+            s.Eating = row['Eating']
+            s.Foraging = row['Foraging']
+            s.Other_Activities = row['Other Activities']
+            s.Kuks = row['Kuks']
+            s.Quaas = row['Quaas']
+            s.Moans = row['Moans']
+            s.Tail_flags = row['Tail flags']
+            s.Tail_twitches = row['Tail twitches']
+            s.Approaches = row['Approaches']
+            s.Indifferent = row['Indifferent']
+            s.Runs_from = row['Runs from']
 
             s.save()
-            break
-
-        # for poll_id in options['path']:
-        #     try:
-        #         poll = Poll.objects.get(pk=poll_id)
-        #     except Poll.DoesNotExist:
-        #         raise CommandError('Poll "%s" does not exist' % poll_id)
-        #
-        #     poll.opened = False
-        #     poll.save()
-
-        self.stdout.write(self.style.SUCCESS(path))
