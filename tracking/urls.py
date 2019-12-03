9d164887f56de2a14f25dd1c1f9c4f9515c83ad3
Inaara Kheraj <isk2117@columbia.edu>
2019-12-03 13:08:13 -0500

    added routing for add, stats, update, and delete

---
+++ tracking/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from tracking import views
+
+urlpatterns = [
+    path('', views.list_sightings),
+    path('add', views.add_sighting),
+    path('stats', views.stats),
+    path('<str:pk>', views.update_or_delete)
+]
