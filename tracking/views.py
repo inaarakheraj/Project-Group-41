380935f6f17244706e9280ba56aa4597c3badb99
Vishakha V <vishakha.b.18@gmail.com>
2019-12-08 22:15:25 -0500

    forms for add and update

--- tracking/views.py
+++ tracking/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from tracking.models import Sighting
 import json
 import numpy as np
+from django import forms
 
 
 def list_sightings(request):
@@ -13,40 +14,64 @@ def list_sightings(request):
 
 def add_sighting(request):
 
-    data = json.loads(request.body.decode('utf-8'))
+    if request.method == "POST":
+        form = SightingForm(request.POST)
+        if form.is_valid():
 
-    s = Sighting()
-    s.Latitude = data.get('Latitude')
-    s.Longitude = data.get('Longitude')
-    s.Unique_Squirrel_ID = data.get('Unique Squirrel ID')
-    s.Shift = data.get('Shift')
-    s.Date = data.get('Date')
-    s.Age = data.get('Age')
-    s.Primary_Fur_Color = data.get('Primary Fur Color')
-    s.Location = data.get('Location')
-    s.Specific_Location = data.get('Specific Location')
-    s.Running = data.get('Running')
-    s.Chasing = data.get('Chasing')
-    s.Climbing = data.get('Climbing')
-    s.Eating = data.get('Eating')
-    s.Foraging = data.get('Foraging')
-    s.Other_Activities = data.get('Other Activities')
-    s.Kuks = data.get('Kuks')
-    s.Quaas = data.get('Quaas')
-    s.Moans = data.get('Moans')
-    s.Tail_flags = data.get('Tail flags')
-    s.Tail_twitches = data.get('Tail twitches')
-    s.Approaches = data.get('Approaches')
-    s.Indifferent = data.get('Indifferent')
-    s.Runs_from = data.get('Runs from')
+            data = form.cleaned_data
 
-    s.save()
+            s = Sighting()
+            s.Latitude = data.get('Latitude')
+            s.Longitude = data.get('Longitude')
+            s.Unique_Squirrel_ID = data.get('Unique_Squirrel_ID')
+            s.Shift = data.get('Shift')
+            s.Date = data.get('Date')
+            s.Age = data.get('Age')
+            s.Primary_Fur_Color = data.get('Primary_Fur_Color')
+            s.Location = data.get('Location')
+            s.Specific_Location = data.get('Specific_Location')
+            s.Running = data.get('Running')
+            s.Chasing = data.get('Chasing')
+            s.Climbing = data.get('Climbing')
+            s.Eating = data.get('Eating')
+            s.Foraging = data.get('Foraging')
+            s.Other_Activities = data.get('Other_Activities')
+            s.Kuks = data.get('Kuks')
+            s.Quaas = data.get('Quaas')
+            s.Moans = data.get('Moans')
+            s.Tail_flags = data.get('Tail_flags')
+            s.Tail_twitches = data.get('Tail_twitches')
+            s.Approaches = data.get('Approaches')
+            s.Indifferent = data.get('Indifferent')
+            s.Runs_from = data.get('Runs_from')
+
+            s.save()
+
+    elif request.method == "GET":
+        # fields = ['Unique Squirrel ID','Latitude', 'Longitude', 'Shift', 'Date', 'Age',
+        #           'Primary Fur Color', 'Location', 'Specific Location',  'Other Activities']
+        #
+        # bool_fields = ['Running', 'Chasing', 'Climbing', 'Eating', 'Foraging', 'Kuks',
+        #                'Quaas', 'Moans','Tail flags', 'Tail twitches', 'Indifferent', 'Runs from']
+
+        return render(request, "tracking/add.html", {'form': SightingForm()})
 
 
 def update_or_delete(request, pk):
 
+    s = Sighting.objects.get(pk=pk)
+
+    if request.method == "GET":
+        form = SightingForm(s.__dict__)
+
+        if form.is_valid():
+            return render(request, "tracking/add.html", {'form': form})
+        else:
+            return render(request, "tracking/add.html", {'errors': form.errors})
+
     # update
     if request.method == "POST":
+
         data = json.loads(request.body.decode('utf-8'))
         s = Sighting.objects.get(pk=pk)
         
@@ -75,9 +100,6 @@ def update_or_delete(request, pk):
         s.Runs_from = data.get('Runs from') or s.Runs_from
         
         s.save()
-    # delete        
-    elif request.method == "DELETE":
-        Sighting.objects.get(pk=pk).delete()
 
 
 def stats(request):
@@ -139,4 +161,32 @@ def map_view(request):
     for sighting in s[:100]:
         sightings.append(Point(sighting[1], sighting[0]))
 
-    return render(request, 'tracking/map.html', {'sightings': sightings})
+    return render(request, 'tracking/map.html', {'sightings': sightings})\
+
+
+
+class SightingForm(forms.Form):
+
+    Unique_Squirrel_ID = forms.CharField(max_length=20,required=False)
+    Latitude = forms.FloatField(required=False)
+    Longitude = forms.FloatField(required=False)
+    Shift = forms.CharField(max_length=2,required=False)
+    Date = forms.CharField(max_length=10,required=False)
+    Age = forms.CharField(max_length=10, required=False)
+    Primary_Fur_Color = forms.CharField(max_length=10,required=False)
+    Location = forms.CharField(max_length=20,required=False)
+    Specific_Location = forms.CharField(max_length=50,required=False)
+    Other_Activities = forms.CharField(max_length=20,required=False)
+    Running = forms.BooleanField(required=False)
+    Chasing = forms.BooleanField(required=False)
+    Climbing = forms.BooleanField(required=False)
+    Eating = forms.BooleanField(required=False)
+    Foraging = forms.BooleanField(required=False)
+    Kuks = forms.BooleanField(required=False)
+    Quaas = forms.BooleanField(required=False)
+    Moans = forms.BooleanField(required=False)
+    Tail_flags = forms.BooleanField(required=False)
+    Tail_twitches = forms.BooleanField(required=False)
+    Approaches = forms.BooleanField(required=False)
+    Indifferent = forms.BooleanField(required=False)
+    Runs_from = forms.BooleanField(required=False)
